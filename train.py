53454fbb5054d42eed68994b5937375a4762f5c9
Tim Kneisel <t.kneisel@tu-bs.de>
2020-04-02 16:26:51 +0200

    Raised frame count for epoch evaluation.

--- train.py
+++ train.py
@@ -269,13 +269,13 @@ def fit(epochs, model, opt, train_dl, dataset, validationset=None):
 
         # Evaluate on train and dev set
         model.eval()
-        train_acc, train_classes = helpers.evaluate(dataset, model, truth_table, ground_truth=ground_truth, device=device, max=256)
+        train_acc, train_classes = helpers.evaluate(dataset, model, truth_table, ground_truth=ground_truth, device=device, max=1024)
         train_acc = round(train_acc, 2)
         writer.add_scalar('train acc', train_acc, epoch + 1)
 
         valid_acc = ''
         if validationset is not None:
-            valid_acc, valid_classes = helpers.evaluate(validationset, model, truth_table, ground_truth=ground_truth, device=device, max=256)
+            valid_acc, valid_classes = helpers.evaluate(validationset, model, truth_table, ground_truth=ground_truth, device=device, max=1024)
             valid_acc = round(valid_acc, 2)
             writer.add_scalar('valid acc', valid_acc, epoch + 1)
 
