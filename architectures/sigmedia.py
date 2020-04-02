e43ce099d6290fdc8bd6571f79898c3ac14a84cc
Tim Kneisel <t.kneisel@tu-bs.de>
2020-04-02 17:40:24 +0200

    Bugfix: trained Sigmedia ResNet10s can now be exported properly.

--- architectures/sigmedia.py
+++ architectures/sigmedia.py
@@ -44,11 +44,15 @@ class Net(torch.nn.Module):
         self.target_size = target_size
         self.cnn_dense_units = cnn_dense_units
         self.ReLU = torch.nn.ReLU().to(device)
+        
         self.layer1 = torch.nn.Sequential(torch.nn.Conv2d(channels, cnn_filters[0], (3, 3), padding=1), torch.nn.BatchNorm2d(cnn_filters[0])).to(device)
-        self.layers = []
-        self.layers.append(residual_block(cnn_filters[0], cnn_filters[0], (3, 3), 1, device, project_shortcut=False, skip_bn=True))
+        
+        layers = []
+        layers.append(residual_block(cnn_filters[0], cnn_filters[0], (3, 3), 1, device, project_shortcut=False, skip_bn=True))
         for ix, num_filters in enumerate(cnn_filters[1:]):
-            self.layers.append(residual_block(cnn_filters[ix], num_filters, (3, 3), 2, device, project_shortcut=True).to(device))
+            layers.append(residual_block(cnn_filters[ix], num_filters, (3, 3), 2, device, project_shortcut=True).to(device))
+        self.layers = torch.nn.Sequential(*layers)
+
         self.final = torch.nn.Conv2d(cnn_filters[-1], cnn_dense_units, (5, 5), 1).to(device)
         self.fc = torch.nn.Sequential(torch.nn.Dropout(dropout_rate), torch.nn.Linear(cnn_dense_units, target_size))
 
@@ -59,8 +63,7 @@ class Net(torch.nn.Module):
         flow = self.ReLU(flow)
 
         # Layers 2-...
-        for l in self.layers:
-            flow = l(flow)
+        flow = self.layers(flow)
         
         # Final ResNet-Layer
         flow = self.final(flow)
