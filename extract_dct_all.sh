f6d87e6625ba40613f3ea1b1fb18e9446ca50b1b
Imbecillus <t.kneisel@tu-bs.de>
2020-04-30 16:06:11 +0200

    Script for extracting DCT feats of all sequences

--- extract_dct_all.sh
+++ extract_dct_all.sh
@@ -1,1 +1,27 @@
-echo "Test"
+echo "Extracting DCT features for everything in $1."
+path=$1
+echo "Number of features: $2"
+n=$2
+
+deltas=""
+if [ $3 = "1" ]
+then
+    echo "Appending deltas"
+    deltas="-d"
+fi
+
+if [ $3 = "2" ]
+then
+    echo "Appending deltas + delta-deltas"
+    deltas="-dd"
+fi
+
+echo "Command: python extract_dct_features.py $path/[SPEAKER]/Clips/ROI/[SEQUENCE]/ $n $deltas"
+
+for d in $path*M/ $path*F/
+do
+    for seq in ${d}Clips/ROI/*/
+    do
+        python extract_dct_features.py $seq $n $deltas -v
+    done
+done
